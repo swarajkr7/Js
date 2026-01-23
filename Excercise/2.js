0f3d293027fe4c6f02d89ff8ad642bc04993d100
Swaraj Kumar <108213393+swarajkr7@users.noreply.github.com>
2026-01-23 16:00:27 +0530

    update

--- Excercise/2.js
+++ Excercise/2.js
@@ -4,6 +4,15 @@ let burger=8;
 let icecream=5;
 
 console.log(soup+burger*3+icecream);
+console.log(soup+burger*3+icecream-3);
+
 
 //second
 console.log((soup+burger*3+icecream)/3);
+
+function averagePrice(soup, burger, icecream){
+    return (soup+burger*3+icecream)/3;
+    
+}
+const average=averagePrice(10,8,5);
+console.log(average);
